ca6dcef93ce59982c831ae336d72bbdcbe43b912
Enrico Mazelli <enrico.mazellit@gmail.com>
2023-10-19 14:53:57 -0300

    working principal tab; textResult logic improved

--- funcs.js
+++ funcs.js
@@ -1,3 +1,6 @@
+// Vars
+let errorMessage = "Invalid"
+
 // Change tabs
 let aTab = document.getElementById("aTab");
 let pTab = document.getElementById("pTab");
@@ -23,7 +26,6 @@ function changeTabs(tab, form, textResult) {
     activeTab.classList.remove("aba-selected");
     activeTab = tab;
     activeTab.classList.add("aba-selected");
-    console.log(forms.style)
     activeTab.id == "aTab" ? forms.style.borderTopLeftRadius = "0px" : forms.style.borderTopLeftRadius = "15px";
     activeTab.id == "mTab" ? forms.style.borderTopRightRadius = "0px" : forms.style.borderTopRightRadius = "15px";
 
@@ -54,31 +56,37 @@ function getRate(rate, perc, aa) {
     return rate;
 }
 
-// Math Functions
+// Useful functions
 function commas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-function Simple(amount, principal, rate, months) {
-    if (amount == null) {
-        
-    } else if (principal == null) {
-
-    } else if (rate == null) {
-        return (amount/principal)**(1/months)-1;
-    } else if (months == null) {
-
-    }
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+// Math Functions
+
+// function Simple(amount, principal, rate, months) {
+//     if (amount == null) {
+        
+//     } else if (principal == null) {
+
+//     } else if (rate == null) {
+//         return (amount/principal)**(1/months)-1;
+//     } else if (months == null) {
+
+//     }
+// }
 
 function Continuous(amount, principal, rate, months, initial) {
     if (amount == null) {
         if (months == 0)
-            return principal
+            return principal;
         else
-            return principal*(1+rate)*(((1+rate)**months-1)/rate)+initial*(1+rate)**months
+            return principal*(1+rate)*(((1+rate)**months-1)/rate)+initial*(1+rate)**months;
     } else if (principal == null) {
-
+        return (amount-initial*(1+rate)**months)*rate/(((1+rate)**months-1)*(1+rate));
     } else if (rate == null) {
 
     } else if (months == null) {
@@ -87,15 +95,16 @@ function Continuous(amount, principal, rate, months, initial) {
 }
 
 function getAmount() {
-    let initial = initialInput.disabled ? 0 : parseFloat(initialInput.value);
+    let initial = initialInput.disabled ? NaN : parseFloat(initialInput.value);
     let principal = parseFloat(document.getElementById("aPrincipal").value);
     let rate = parseFloat(document.getElementById("aRate").value);
     let perc = document.getElementById("aPerc").checked;
     let aa = document.getElementById("aAA").checked;
     let years = parseFloat(document.getElementById("aYears").value);
     let months = parseFloat(document.getElementById("aMonths").value);
+    // console.log(initial, principal, rate, perc, aa, years, months);
 
-    console.log(initial, principal, rate, perc, aa, years, months);
+    if (isNaN(initial) && isNaN(principal) || isNaN(rate) || isNaN(years) && isNaN(months)) return errorMessage;
 
     if (isNaN(initial)) initial = 0;
     if (isNaN(principal)) principal = 0;
@@ -103,7 +112,29 @@ function getAmount() {
     if (isNaN(months)) months = 0;
 
     let amount = Continuous(null, principal, getRate(rate, perc, aa), years*12+months, initial);
+    if (isNaN(amount)) return errorMessage;
     return "R$"+commas((Math.round(amount*100)/100).toFixed(2));
+}
+
+function getPrincipal() {
+    let initial = initialInput.disabled ? NaN : parseFloat(initialInput.value);
+    let amount = parseFloat(document.getElementById("pAmount").value);
+    let rate = parseFloat(document.getElementById("pRate").value);
+    let perc = document.getElementById("pPerc").checked;
+    let aa = document.getElementById("pAA").checked;
+    let years = parseFloat(document.getElementById("pYears").value);
+    let months = parseFloat(document.getElementById("pMonths").value);
+    // console.log(initial, amount, rate, perc, aa, years, months);
+
+    if (isNaN(amount) || isNaN(rate) || isNaN(years) && isNaN(months)) return errorMessage;
+
+    if (isNaN(initial)) initial = 0;
+    if (isNaN(years)) years = 0;
+    if (isNaN(months)) months = 0;
+
+    let principal = Continuous(amount, null, getRate(rate, perc, aa), years*12+months, initial);
+    if (isNaN(principal)) return errorMessage;
+    else return "R$"+commas((Math.round(principal*100)/100).toFixed(2));
 }
 
 // Calc Button
@@ -112,9 +143,12 @@ let calcBtn = document.getElementById("calcBtn");
 calcBtn.onclick = () => {
     switch (activeTab.id) {
         case "aTab":
-            aTextResult.textContent = getAmount();
+            aTextResult.textContent = "";
+            sleep(80).then(() => aTextResult.textContent = getAmount());
             break;
         case "pTab":
+            pTextResult.textContent = "";
+            sleep(80).then(() => pTextResult.textContent = getPrincipal());
             break;
         case "rTab":
             break;
